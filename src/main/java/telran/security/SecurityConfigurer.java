67fc13a6c7fad5c897fa74837ddea0324c5e15c7
Yuragranovsky <yuragranovsky@gmail.com>
2022-07-17 21:15:04 +0300

    initial code security-library

---
+++ src/main/java/telran/security/SecurityConfigurer.java
@@ -0,0 +1,34 @@
+package telran.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+	@Value("${app.security.enable:true}")
+	boolean securityEnable;
+	@Bean
+	PasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic();
+		http.cors().and().csrf().disable();
+		if (securityEnable) {
+			http.authorizeHttpRequests().antMatchers("/login").permitAll();
+			http.authorizeHttpRequests().antMatchers(HttpMethod.GET).hasAnyRole("USER", "ADMIN");
+			http.authorizeHttpRequests().anyRequest().hasRole("ADMIN");
+		} else {
+			http.authorizeHttpRequests().anyRequest().permitAll();
+		}
+	}
+
+}
